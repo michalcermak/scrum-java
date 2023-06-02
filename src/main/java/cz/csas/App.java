5e315353c5b2c7dc96ce10bda276c33003dedac6
Čermák Michal <mcermak@ext.csas.cz>
2023-06-02 15:32:29 +0200

    App skeleton

---
+++ src/main/java/cz/csas/App.java
@@ -0,0 +1,14 @@
+package cz.csas;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(getHelloMessage());
+    }
+
+    public static String getHelloMessage() {
+        return "Hello, world!";
+    }
+}
